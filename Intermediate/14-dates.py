2b9d754f850aee46fda3b050475cb384379e8d91
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-25 10:58:57 -0300

    fix/ folder order

---
+++ Intermediate/14-dates.py
@@ -0,0 +1,17 @@
+## Dates
+
+
+import datetime
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+print(now.date())
+print(now.ctime())
+print(now.day)
+print(now.month)
+print(now.year)
+print(now.hour)
+print(now.minute)
+print(now.second)
+
