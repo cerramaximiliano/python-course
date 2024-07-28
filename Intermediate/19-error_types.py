db75972281e9511f9ecee88dfb6a1bcda1be117a
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-28 16:53:55 -0300

    feature/ errors in python

---
+++ Intermediate/19-error_types.py
@@ -0,0 +1,36 @@
+### Error  Types ###
+
+print("**** SyntaxError ****")
+## print "hola"
+
+print("**** NameError ****")
+## print(name)
+
+print("**** IndexError ****")
+my_list = ["Python", "Swift", "Kotlin", "Dart", "Javascript"]
+## print(my_list[10])
+
+print("**** ModuleNotFoundError ****")
+##import maths
+
+print("**** AttributeError ****")
+import math
+#print(math.PI)
+
+print("**** KeyError ****")
+my_dict = dict()
+my_dict = {"name": "Max", "age": 40}
+#print(my_dict["names"])
+
+print("**** TypeError ****")
+#print(my_list["name"])
+
+print("**** ImportError ****")
+#from math import PI
+
+print("**** ValueError ****")
+#my_int = int("10d")
+
+print("**** ZeroDivisionError ****")
+#print(5/0)
+
