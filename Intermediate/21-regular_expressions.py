3077abf695e61f22b8f27856b187dea815ae70ae
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-29 09:36:58 -0300

    feature/ regular expressions

--- Intermediate/21-regular_expressions.py
+++ Intermediate/21-regular_expressions.py
@@ -1,2 +1,9 @@
 ### Regular Expressions ###
 
+import re
+
+my_string = "Esta es la lección 21 del curso de Python"
+my_other_string = "Esta no es la lección 21"
+
+print(re.match("Esta es la lección", my_string))
+print(re.match("Esta es la lección", my_other_string))
