3ce3d3008049a4646e577d076770f35cbe10e343
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-30 09:54:07 -0300

    feature/ re search findall split

--- Intermediate/21-regular_expressions.py
+++ Intermediate/21-regular_expressions.py
@@ -3,10 +3,27 @@
 import re
 
 my_string = "Esta es la lección 21 del curso de Python"
-my_other_string = "Esta no es la lección 21"
+my_other_string = "Esta no es la lección 21: lección 1"
 
 match = re.match("Esta es la lección", my_string, re.I)
 print(re.match("Esta es la lección", my_other_string))
 
-span = match.span()
 
+
+if match != None:
+    print(match)
+    span = match.span()
+    print(match.span())
+    start, end = span
+    print(my_string[start:end])
+
+
+search = re.search("Esta es la lección", my_string, re.I)
+print(search)
+
+email = "cerramaximiliano@gmail.com"
+
+findall = re.findall("lección", my_string, re.I)
+print(findall)
+split = re.split(":", my_other_string)
+print(split)
