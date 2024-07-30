3b78c1f026983825022cb3ff8446b9eab966af52
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-30 09:37:02 -0300

    feature/ regular expressions

--- Intermediate/21-regular_expressions.py
+++ Intermediate/21-regular_expressions.py
@@ -5,5 +5,8 @@ import re
 my_string = "Esta es la lección 21 del curso de Python"
 my_other_string = "Esta no es la lección 21"
 
-print(re.match("Esta es la lección", my_string))
+match = re.match("Esta es la lección", my_string, re.I)
 print(re.match("Esta es la lección", my_other_string))
+
+span = match.span()
+
