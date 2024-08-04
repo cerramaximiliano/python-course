5b2d25f85b6d7a638e13a52ebefcace8f60af681
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-08-04 12:26:46 -0300

    feature/ packages

--- Intermediate/22-packages.py
+++ Intermediate/22-packages.py
@@ -4,6 +4,19 @@ print("Python")
 
 import numpy
 print(numpy.version.version)
-numpy.array([1,2,3,5,7,2,4,8])
+numpy_array = numpy.array([1,2,3,5,7,2,4,8])
+print(type(numpy_array))
+print(numpy_array * 2)
 
+import pandas
+print(pandas)
 
+import requests
+print(requests)
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+print(response)
+print(response.status_code)
+print(response.json())
+
+from mypackage import arithmetics
+print(arithmetics.sum_two_values(1, 4))
